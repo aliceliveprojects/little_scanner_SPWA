52bc3abf01bb2e2d72d4e0bf37bd62bd5b80fbf9
Athul Philip <athulphilip1309@gmail.com>
2018-12-04 18:00:44 +0000

    Added comments

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 # little_scanner_SPWA
 
-A simple QR Scanner SPWA demo using AngularJS and the "qr-scanner" library.
+A simple QR Scanner SPWA demo using AngularJS and the "instascan" library.
 
-[Link to qr-scanner repository](https://github.com/nimiq/qr-scanner)
+[Link to instascan repository](https://github.com/schmich/instascan)
--- js/app.js
+++ js/app.js
@@ -10,6 +10,7 @@
 
     function control() {
         var vm = angular.extend(this, {
+            // add scanner object to data model
             scanner: new Instascan.Scanner({
                 video: document.getElementById('qr-video'),
                 continuous: true,
@@ -21,27 +22,27 @@
 
         vm.scanner.addListener('scan', function (content) {
             alert(content);
+
+            // stop the scanner as it should only scan one barcode each time
             vm.scanner.stop();
         });
 
         vm.scanBarcode = function () {
 
+            // get a camera, pass it into the start function of the scanner object in the data model (defined above)
+            // this will start the camera, show the video feed on the page and start scanning for a barcode
             Instascan.Camera.getCameras().then(function (cameras) {
-
                 if (cameras.length > 0) {
                     vm.scanner.start(cameras[0]);
                     console.log("started scanner");
                 } else {
                     console.error('No cameras found.');
                 }
-
             }).catch(function (e) {
                 console.error(e);
             });
 
         };
-
-        return vm;
     }
 
 })();
